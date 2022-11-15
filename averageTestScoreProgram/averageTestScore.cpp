4488b28e81c7ced02344a1ef17ae458a088d0aa5
Imam Bagus Nugroho <imambagus9864@gmail.com>
2022-11-15 16:45:31 +0700

    Add rectangle, triangle & average test score program

---
+++ averageTestScoreProgram/averageTestScore.cpp
@@ -0,0 +1,28 @@
+/*
+program calculates average test score given individual
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int testScore;
+    int numStudent;
+    double average;
+    double testTotal;
+
+    cout << "Enter a number student :";
+    cin >> numStudent;
+
+    for (int i = 1; i <= numStudent; i++)
+    {
+        cout << "Enter a test score student number " << i << " : ";
+        cin >> testScore;
+        testTotal += testScore;
+    }
+
+    average = testTotal / numStudent;
+    cout << "Number of student  : " << numStudent << endl;
+    cout << "Average Test Score : " << average << endl;
+}
